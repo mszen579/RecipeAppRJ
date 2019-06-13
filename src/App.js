cb0fd0737116e5bc6cd1f6fa67104cce7a07485f
unknown <mszen579@hotmail.com>
2019-06-13 17:33:10 +0200

    v5 commit

--- src/App.js
+++ src/App.js
@@ -12,21 +12,23 @@ const api_key="f39b77adf0514badcf2fc13d45963b2e";
 class App extends Component {
   state={
     recipes : [],
-
   }
+
 
 getRecipe = async (event) =>{
   const recipeName = event.target.elements.recipeName.value;
   event.preventDefault();
 // I had to add https://cors-anywhere.herokuapp.com/ before the api URL because some api servers do not allow localhost call so with this we trick these type of servers
-  const api_call= await fetch(`http://food2fork.com/api/search?key=${api_key}&q=${recipeName}`);
+  const api_call= await fetch(`https://cors-anywhere.herokuapp.com/https://food2fork.com/api/search?key=${api_key}&q=${recipeName}`);
   const data = await api_call.json();
   console.log(data);
   this.setState({recipes:data.recipes});
   console.log(this.state.recipes)
 }
 
-  render() {
+
+  
+render() {
     return (
       <div className="App">
         <header className="App-header">
--- src/components/SingleRecipe.js
+++ src/components/SingleRecipe.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import {Link} from 'react-router-dom'
 
 const API_KEY = "f39b77adf0514badcf2fc13d45963b2e";
 
@@ -11,7 +11,7 @@ export default class SingleRecipe extends Component {
        const title = this.props.location.state.recipe;
      
         // I had to add https://cors-anywhere.herokuapp.com/ before the api URL because some api servers do not allow localhost call so with this we trick these type of servers
-        const req= await fetch(`http://food2fork.com/api/search?key=${API_KEY}&q=${title}`);
+        const req= await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${title}`);
         const res = await req.json();
         // console.log(res.recipes[0]);
         this.setState({activeRecipe: res.recipes[0]})
@@ -24,7 +24,11 @@ export default class SingleRecipe extends Component {
                 <div className="active-recipe">
                     <img class="active-recipe__img" src={recipe.image_url} alt={recipe.title}/>
                     <h3 class="active-recipe__title">{recipe.title}</h3>
-                    <h4 class="active-recipe__publisher">{recipe.publisher}</h4>
+                    <h5 class="active-recipe__publisher">{recipe.publisher}</h5>
+                    <p class="active-recipe__website">Website: {recipe.publisher} <span> | <a href={recipe.publisher_ur}>{recipe.publisher_url}</a></span></p>
+                    <button className="active-recipe__button">
+                      <Link to="/">Home Page</Link>  
+                        </button>
                 </div>
             </div>
         )
